f4a8f34fa3145fea0166e5ca1ff79da18b0af920
MinhDuc098 <86830154+MinhDuc098@users.noreply.github.com>
2022-02-08 22:13:06 +0700

    Add files via upload

---
+++ JavaScript/javascript.js
@@ -0,0 +1,6 @@
+function onmouse(a){
+    //document.write("dit me xem sex còn ngồi soi nội dung rảnh vcl, click thì click mẹ đi");
+    //if(a==1) alert("á à tóm được một thằng đang xem sẽ nhớ");
+    //if(a==2) alert("thích thì click mẹ đi cứ di di");
+    //if(a==3) document.write("your computer has been invaded by viruss");
+}
